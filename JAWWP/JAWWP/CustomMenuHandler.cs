5659c1a1e1e960114bbcf50f6b66b10c08ac814f
Juan Ángel López Sanz <juanangel.lopezs@gmail.com>
2015-10-16 00:31:28 +0200

    First Commit

---
+++ JAWWP/JAWWP/CustomMenuHandler.cs
@@ -0,0 +1,49 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JAWWP
+{
+    public class CustomMenuHandler : IContextMenuHandler
+    {   
+        /// <summary>
+        /// Clear context (right mouse click) Menu.
+        /// </summary>
+        /// <param name="browserControl">-</param>
+        /// <param name="browser">-</param>
+        /// <param name="frame">-</param>
+        /// <param name="parameters">-</param>
+        /// <param name="model">-</param>
+        void IContextMenuHandler.OnBeforeContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model)
+        {
+            bool result = model.Clear();
+        }
+        /// <summary>
+        /// Do something when menu command detected.
+        /// </summary>
+        /// <param name="browserControl"></param>
+        /// <param name="browser"></param>
+        /// <param name="frame"></param>
+        /// <param name="parameters"></param>
+        /// <param name="commandId"></param>
+        /// <param name="eventFlags"></param>
+        /// <returns></returns>
+        bool IContextMenuHandler.OnContextMenuCommand(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, CefMenuCommand commandId, CefEventFlags eventFlags)
+        {
+            return false;
+        }
+        /// <summary>
+        /// Do nothing
+        /// </summary>
+        /// <param name="browserControl"></param>
+        /// <param name="browser"></param>
+        /// <param name="frame"></param>
+        void IContextMenuHandler.OnContextMenuDismissed(IWebBrowser browserControl, IBrowser browser, IFrame frame)
+        {
+
+        }
+
+    }
+}
---
+++ JAWWP/JAWWP/Form1.cs
@@ -0,0 +1,62 @@
+﻿
+using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace JAWWP
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Ctor Main Window
+        /// 
+        /// JAWWP: JUST ANOTHER WHATSAPP WEB PORT
+        /// AUTHOR: JUAN ANGEL LOPEZ - DHURZO
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// FIRED WHEN FORM IS LOAD.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnLoad(object sender, EventArgs e)
+        {
+            InitializeComponent();
+           
+            CefSettings settings = new CefSettings();
+            settings.CachePath = "./Cookies";
+            settings.PersistSessionCookies = true;
+
+            Cef.Initialize(settings);
+
+
+            CefSharp.WinForms.ChromiumWebBrowser cBrowser = new CefSharp.WinForms.ChromiumWebBrowser("https://web.whatsapp.com")
+            {
+                Dock = DockStyle.Fill,
+                ContextMenu = new ContextMenu(),
+                ContextMenuStrip = new ContextMenuStrip(),
+                MenuHandler =  new CustomMenuHandler(),
+            };
+
+
+            this.Controls.Add(cBrowser);
+
+        }
+
+
+
+
+
+    }
+}
